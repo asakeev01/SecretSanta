2f1335160276c38efb3d9fb92717de24152916bc
Adilet Sydykaliev <adilet.sydykaliev@iaai.edu.kg>
2023-12-04 09:46:43 +0600

    Debug False

--- SecretSanta/SecretSanta/settings.py
+++ SecretSanta/SecretSanta/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2qhz)4_a4g-xfsinubj1yaju85ydjtde-=l&be$ts-aql(0*%x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
