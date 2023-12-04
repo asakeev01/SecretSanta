0b6a394bd73e3f4075f669e6a8a1c2343624bbab
Adilet Sydykaliev <adilet.sydykaliev@iaai.edu.kg>
2023-12-04 10:01:46 +0600

    Changes

--- SecretSanta/SecretSanta/settings.py
+++ SecretSanta/SecretSanta/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -110,7 +111,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- SecretSanta/SecretSanta/urls.py
+++ SecretSanta/SecretSanta/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from SecretSanta import settings
 from . import views
 
 urlpatterns = [
@@ -9,3 +12,6 @@ urlpatterns = [
     path('test_token', views.test_token),
     path("users/", include("users.urls")),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
