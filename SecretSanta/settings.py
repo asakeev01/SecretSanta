55fe1e11515438ab2426fac8e7e61324cbc02175
Adilet Sydykaliev <adilet.sydykaliev@iaai.edu.kg>
2023-12-05 16:18:42 +0600

    Allowed hosts all

--- SecretSanta/settings.py
+++ SecretSanta/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-2qhz)4_a4g-xfsinubj1yaju85ydjtde-=l&be$ts-aql(0*%x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
